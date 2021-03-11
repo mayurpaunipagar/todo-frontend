bdf3f4e3920742f8756d9fba4eb13a3e9ab61b84
MayurHpNotebook <001mayur94@gmail.com>
2021-03-11 17:57:04 +0530

    logout function added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginForm from './components/LoginForm';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [loggedIn,setLoggedIn]=useState(false);
   const [error,setError]=useState(undefined);
+  const [userName,setUserName]=useState(undefined);
+
   const signupHandler=(username,password)=>{
     loginSignUp('https://todo-backend-mayur.herokuapp.com/signup',
             username,
@@ -34,12 +36,45 @@ function App() {
     })
     .then((r)=>{
       if(r.success===true){
-        
+        return getUserName();
       }else{
         setError(r.err);
       }
     })
   }
+
+  const getUserName=()=>{
+    return fetch('https://todo-backend-mayur.herokuapp.com/userinfo',{credentials:"include"})
+    .then(r=>{
+      if(r.ok){
+        return r.json();
+      } else {
+        setLoggedIn(false);
+        setUserName(undefined);
+        return {success:false};
+      }
+    }).then(r=>{
+      if(r.success!==false){
+        setLoggedIn(true);
+        setUserName(r.userName);
+      }
+    });
+  }
+  
+  const logoutHandler=()=>{
+    return fetch('https://todo-backend-mayur.herokuapp.com/logout',{credentials:'include'})
+    .then(r=>{
+      if(r.ok) {
+        setLoggedIn(false);
+        setUserName(undefined);
+      }
+    })
+  }
+
+  useEffect(()=>{
+    getUserName();
+  },[]);
+
   return (
     <div className="App">
       <h1>Mayur's Todo App</h1>
