041810a56ca0f2241cd77d957c15ff9122ad36f5
MayurHpNotebook <001mayur94@gmail.com>
2021-03-11 16:32:05 +0530

    LoginForm component added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [loggedIn,setLoggedIn]=useState(false);
   return (
     <div className="App">
       <h1>Mayur's Todo App</h1>
+      {loggedIn?<h2>My todo list</h2>:
+      <LoginForm/>}
     </div>
   );
 }
